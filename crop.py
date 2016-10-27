b11b471708c6f5ab7a26a46df812bb99a92562ce
Patrick Pan <patrickpan@college.harvard.edu>
2016-10-26 23:28:50 -0400

    Crop to key signature

--- crop.py
+++ crop.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import misc
 
 # Tests if row in image has at least `threshold` pixels that are not `value`.
-def test_row(img, row_number = 0, threshold = 10, **kwargs):
+def test_row(img, row_number = 0, threshold = 10):
     row = (img[row_number] == 255)
     count = np.count_nonzero(row == False)
     # print "row %d count %d" % (row_number, count)
@@ -10,6 +10,7 @@ def test_row(img, row_number = 0, threshold = 10, **kwargs):
 
 # Identifies continuous of size `min_size` regions of the image where `test_row` is true.
 def filled_ranges(img, min_size = 15, threshold = 20):
+    print threshold
     ranges = []
     found_row = False
     for i in range(0, img.shape[0]):
@@ -37,8 +38,7 @@ def crop_to_widest(img, min_size = 15, threshold = 20):
     widest = biggest_diff(filled_ranges(img, min_size, threshold))
     return img[widest[0]:widest[1]]
 
-def split_rows(img, min_size = 15, threshold = 20):
-    ranges = filled_ranges(img, min_size, threshold)
+def split_rows(img, ranges):
     return map(lambda range: img[range[0]:range[1]], ranges)
 
 def save_imgs(images, path = "out/"):
@@ -46,3 +46,6 @@ def save_imgs(images, path = "out/"):
     for img in images:
         misc.imsave("%s%04d.jpg" % (path, i), img)
         i += 1
+
+def percent_filled(img):
+    return float(np.count_nonzero(img != 255)) / float(img.size)
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import numpy as np
 from scipy import misc
 import crop 
@@ -5,15 +7,44 @@ import crop
 imread = misc.imread
 imsave = misc.imsave
 
-for file in range(0, 507):
+files = glob.glob('out/*')
+for f in files:
+    os.remove(f)
+
+errors = 0
+
+for file in range(0, 10):
     my_img = imread('./files/%04d.jpg' % file)
     my_img = crop.remove_top_segment(my_img)
 
     my_img = np.transpose(my_img)
-    my_img = crop.crop_to_widest(my_img, threshold = 300)
-    my_img = np.transpose(my_img)
+    # LANDSCAPE #
 
-    if my_img.shape[1] > 1850:
+    my_img = crop.crop_to_widest(my_img, threshold = 400)
+
+    if my_img.shape[0] > 1850:
         print "Likely error: %04d.jpg" % file
+        errors += 1
 
-    misc.imsave("out/%04d.jpg" % file, my_img)
+    # my_img = my_img[0:250]
+    clef = my_img[0:70]
+
+    my_img = np.transpose(my_img)
+    clef = np.transpose(clef)
+    # PORTRAIT #
+
+    i = 0
+    clef_ranges = crop.filled_ranges(clef, threshold = 1)
+    rows = crop.split_rows(my_img, clef_ranges)
+
+    for row in rows:
+        end = np.transpose(np.transpose(row)[-200:])
+        if crop.percent_filled(np.transpose(row)[-200:]) < 0.02:
+            pass
+        else:
+            misc.imsave("out/%04d-%02d.jpg" % (file, i), row[0:250])
+        i += 1
+
+    # misc.imsave("out/%04d.jpg" % file, my_img)
+
+    print "%d total errors" % errors
